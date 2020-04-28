406b7ce8c3bd08416ec6aca574734ce1740a38d4
Illya Koprjuk <illyakoprjuk@MacBook-Pro-Illya.local>
2020-04-28 10:49:31 +0300

    Updates

---
+++ precache-manifest.41bd60b4c9f8ed0ca79036e57bd40806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af8ac3bafab2990824a3b3560ea3048",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "1255df2e2e15d1a2b7f7",
+    "url": "/goit-react-hw-02-feedback/static/css/main.d2c38b0e.chunk.css"
+  },
+  {
+    "revision": "1a6fddfb44c1c81af929",
+    "url": "/goit-react-hw-02-feedback/static/js/2.4812d37d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.4812d37d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1255df2e2e15d1a2b7f7",
+    "url": "/goit-react-hw-02-feedback/static/js/main.d156b463.chunk.js"
+  },
+  {
+    "revision": "921f1dce578c37a88de9",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.912650e5.js"
+  }
+]);
